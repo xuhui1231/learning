78c97b4dea2e61baada98f31cafb4ee8408dd848
xuhui <xuhui@3h1china.com>
2022-07-26 10:31:29 +0800

    frist commit

---
+++ src/main/java/com/git/learn/GitTest.java
@@ -0,0 +1,16 @@
+package com.git.learn;
+
+/**
+ * @author Xuhui
+ * @title: GitTest
+ * @projectName learning
+ * @description: TODO
+ * @date 2022/7/2610:26
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
